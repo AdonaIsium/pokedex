c372f36cc925edf9b0f495e5aa1d97c90b27d6d0
AdonaIsium <michael.tau21@gmail.com>
2025-01-26 12:50:01 -0600

    added help menu which loops over mapped commands list

--- repl.go
+++ repl.go
@@ -7,15 +7,41 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name 		string
+	description string
+	callback 	func() error
+}
+
+var commands map[string]cliCommand
+
+
+func init(){
+	commands = map[string]cliCommand{
+		"exit": {
+			name: 	"exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+		"help": {
+			name: 	"help",
+			description: "Displays a help menu",
+			callback: commandHelp,
+		},
+	}
+}
+
 func startRepl() {
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		if scanner.Scan(){
 			text := scanner.Text()
 			cleanText := cleanInput(text)
-			if len(cleanText) > 0 {
-				fmt.Printf("Your command was: %s\n", cleanText[0])
+			currentCommand := cleanText[0]
+			if cmd, exists := commands[currentCommand]; exists{
+				cmd.callback()
 			}
 		}
 	}
@@ -26,3 +52,25 @@ func cleanInput(text string) []string {
 	words := strings.Fields(output)
 	return words
 }
+
+func commandExit() error {
+	fmt.Printf("Closing the Pokedex... Goodbye!\n")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	getCommands()
+	return nil
+}
+
+func getCommands() error {
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+
+
+
