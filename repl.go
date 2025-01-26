65c3606f98fac12ed5213125fccfc37b721ce325
AdonaIsium <michael.tau21@gmail.com>
2025-01-26 12:12:52 -0600

    added repl basic functionality

--- repl.go
+++ repl.go
@@ -1,8 +1,25 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan(){
+			text := scanner.Text()
+			cleanText := cleanInput(text)
+			if len(cleanText) > 0 {
+				fmt.Printf("Your command was: %s\n", cleanText[0])
+			}
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
